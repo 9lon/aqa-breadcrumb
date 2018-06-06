b9bd7412d602c0381a64abb934a42fbb0fea7731
Tonsai jompun <tonsai.j@nexts-corp.com>
2018-06-06 11:43:43 +0700

    update to @9lon

--- aqa-breadcrumb.js
+++ aqa-breadcrumb.js
@@ -1,6 +1,6 @@
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 import '@polymer/polymer/lib/elements/dom-repeat.js';
-import '@nylon/aqa-font/aqa-mitr-font.js';
+import '@9lon/aqa-font/aqa-mitr-font.js';
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 class AqaBreadcrumb extends PolymerElement {
   static get template() {
